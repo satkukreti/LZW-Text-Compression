764adf6e28460b08e7bf7ebe09947e7a084e3038
antqiu <anthonyqiuchu@gmail.com>
2024-04-14 22:22:17 -0400

    Add encoder.py

---
+++ compression.py
@@ -0,0 +1,24 @@
+input_file = open("input.txt", "r")
+
+output = []
+string_table = {chr(i):i for i in range(32, 127)}
+string_table[chr(10)] = 10
+count = 128
+current = ""
+
+while True:
+    char = input_file.read(1)
+    if not char:
+        if current:
+            output.append(string_table[current])
+        break
+
+    current += char
+    if current not in string_table.keys():
+        string_table[current] = count
+        output.append(string_table[current[:-1]])
+        current = char
+        count += 1
+
+print(len(output) + len(string_table))
+# print(string_table)
