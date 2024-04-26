e341e3965c3391e38a51151351cfd6f5a05cc124
antqiu <anthonyqiuchu@gmail.com>
2024-04-26 18:47:15 -0400

    packed output files

--- compression.py
+++ compression.py
@@ -1,5 +1,10 @@
-user_file = input("Please enter your file name: ")
+from struct import *
+
+user_file = input("Please enter your input file name: ")
 input_file = open(user_file, "r")
+
+user_file_output = input("Please enter your output file name: ")
+output_file = open(user_file_output, "wb")
 
 output = []
 string_table = {chr(i):i for i in range(32, 127)}
@@ -15,11 +20,22 @@ while True:
         break
 
     current += char
+    
+    # if len(string_table) > 4096:
+    #     break 
+
     if current not in string_table.keys():
         string_table[current] = count
         output.append(string_table[current[:-1]])
         current = char
         count += 1
 
-print(len(output) + len(string_table))
-# print(string_table)
+stringed_string_table = ''.join(chr(string_table[key]) for key in sorted(string_table.keys()))
+output_file.write(pack('>H', len(stringed_string_table)))
+output_file.write(stringed_string_table.encode('utf-8'))
+
+for data in output:
+    output_file.write(pack('>H', data))
+
+input_file.close()    
+output_file.close()
