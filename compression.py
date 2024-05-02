275c732000c92e81150909f146e6e904947004a7
satkukreti <satkukreti@gmail.com>
2024-05-02 02:01:52 -0400

    fixed decryption in compression.py

--- compression.py
+++ compression.py
@@ -9,6 +9,10 @@ output_file = open(user_file_output, "wb")
 output = []
 string_table = {chr(i):i for i in range(32, 127)}
 string_table[chr(10)] = 10
+
+rstring_table = {i:chr(i) for i in range(32, 127)}
+rstring_table[10] = chr(10)
+
 count = 128
 current = ""
 
@@ -26,6 +30,7 @@ while True:
 
     if current not in string_table.keys():
         string_table[current] = count
+        rstring_table[count] = current
         output.append(string_table[current[:-1]])
         current = char
         count += 1
@@ -39,3 +44,21 @@ for data in output:
 
 input_file.close()    
 output_file.close()
+
+
+print("Testing lossless decryption...\n")
+
+decompressed_data = []
+
+for i in output:
+    if i in rstring_table:
+        decompressed_data.append(rstring_table[i])
+
+# Join the list of characters into a string
+decompressed_text = ''.join(decompressed_data)
+
+# Write the decrypted text to a file
+with open("decrypt.txt", "w") as decrypt_file:
+    decrypt_file.write(decompressed_text)
+
+print("Decryption completed. Decrypted data written to 'decrypt.txt'.")
